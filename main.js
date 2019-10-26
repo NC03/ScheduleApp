4901a0353123e9dc28e556a7baf81cf495b34814
N C <>
2019-10-26 10:59:37 -0700

    working

--- main.js
+++ main.js
@@ -9,17 +9,29 @@ function createWindow() {
         width: 800,
         height: 600
     })
+
+    console.log(process)
     win.loadFile('index.html')
+
     win.webContents.openDevTools()
-    
+
+    win.on("ready", function () {
+
+    })
+
     win.on('closed', function () {
+
         win = null
     })
 }
+
+
+app.on('ready', createWindow)
+
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-    if (mainWindow === null) createWindow()
+    if (win === null) createWindow()
 })
