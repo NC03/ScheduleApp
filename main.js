0c969607bb4663ddea896916258eecf8df2601de
NC03 <nc03developer@gmail.com>
2020-01-11 20:36:08 -0800

    no dev tools

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function createWindow() {
 	});
 
 	win.loadFile(path.join(__dirname, "index.html"));
-	win.webContents.openDevTools();
+	// win.webContents.openDevTools();
 	win.on("closed", function() {
 		win = null;
 	});
